d2d52fcff8a4a54e1f9df7f694204211fc8d98cb
Evan Riddick <riddickevan841@gmail.com>
2023-04-20 10:55:13 -0400

    Add Modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+let body = document.querySelector(`body`);
 let main = document.getElementsByTagName(`main`)[0];
 let jsTriggers = document.getElementById(`js-triggers`);
 let menu = document.getElementsByTagName(`ul`)[0];
@@ -7,33 +8,58 @@ let modalPanel = document.getElementById(`modal-panel`);
 
 
 showMenu.addEventListener(`click`, () => {
-    let menuCounter = 0;
+    const menuCounter = menu.style.zIndex;
     if (menuCounter == 0) {
         console.log(`Menu Opened`);
-        menu.style.top = 25 + `px`;
-        menuCounter = menuCounter++;
+        menu.style.top = 30 + `px`;
+        menu.style.zIndex++;
     }
-    if (menuCounter == 2) {
+    if (menu.style.zIndex == 2) {
         console.log(`Menu Closed`);
-        menu.style.top = -200 + `px`;
-        // menu2.style.top = 200 + `px`;
-        menuCounter = 0;
+        menu.style.top = -30 + `px`;
+        menu.style.zIndex = 0;
     }
 });
 
 showModal.addEventListener(`click`, () => {
     console.log(`Modal Open`);
-    main.style.opacity = 0.1;
-    modalPanel.style.top = 25 + `px`;
+    body.style.backgroundColor = `rgba(0,0,0,0.5)`;
+    jsTriggers.style.backgroundColor = `rgba(0,0,0,0.5)`;
     modalPanel.style.opacity = 1;
+    modalPanel.style.top = 25 + `px`;
+    modalPanel.style.zIndex = 2;
+});
+
+main.addEventListener(`click`, () => {
+    if (modalPanel.style.zIndex == 2) {
+        console.log(`Modal Closed`);
+        body.style.backgroundColor = `rgba(0,0,0,0)`;
+        jsTriggers.style.backgroundColor = `rgba(0,0,0,0)`;
+        modalPanel.style.opacity = 1;
+        modalPanel.style.top = -700 + `px`;
+        modalPanel.style.zIndex = 0;
+    }
+});
+
+document.addEventListener(`keydown`, (e) => {
+    if (e.key == `Escape`) {
+        if (modalPanel.style.zIndex == 2) {
+            console.log(`Modal Closed`);
+            body.style.backgroundColor = `rgba(0,0,0,0)`;
+            jsTriggers.style.backgroundColor = `rgba(0,0,0,0)`;
+            modalPanel.style.opacity = 1;
+            modalPanel.style.top = -700 + `px`;
+            modalPanel.style.zIndex = 0;
+        }
+    }
 });
 
 
 jsTriggers.appendChild(menu);
 
 // body.addEventListener(`click`, () => {
-//     if (modal.style.opacity == 1) {
+//     if (modal.style.opacity =q= 1) {
 //         body.style.opacity = 1;
-//         modal.style.zIndex = -1;
+//         modalPanel.style.zIndex = -1;
 //     }
 // });
